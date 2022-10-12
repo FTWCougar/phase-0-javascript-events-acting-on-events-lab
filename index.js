38488d11218e48319a745272c71b00a8e441cffe
Cameron <Cameron@dansclan.com>
2022-10-12 13:32:53 -0700

    commit tree 374d8625c8c570b9563c4eb590171475fde345c8

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+let speed = 0;
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (speed < 20)[
+    speed++
+    ]
+  if (left > 0) {
+    dodger.style.left = `${left - speed}px`;
+  }
+}
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (speed < 20)[
+        speed++
+    ]
+    if (left < 360) {
+      dodger.style.left = `${left + speed}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === `ArrowRight`){
+    moveDodgerRight();
+  }
+});
+document.addEventListener(`keyup`, function (){
+    speed = 0;
+})
